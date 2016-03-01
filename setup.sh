b620017cf29b48f76aee65a11ab1aa3b7dd4e827
slgu <blackhero98@gmail.com>
2016-02-29 19:59:11 -0500

    modify setupsh

--- setup.sh
+++ setup.sh
@@ -5,7 +5,8 @@
 # Created Time: Mon Feb 29 19:36:24 2016
 #########################################################################
 #!/bin/bash
-conda install numpy=1.9.3
+conda install numpy=1.10.4
 conda install scipy==0.15.1
 conda install matplotlib==1.4.2
 pip install theano
+conda install libgfortran
